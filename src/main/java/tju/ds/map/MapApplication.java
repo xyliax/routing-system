d84d158546bc18fd56a7295146bf6c51a293a8cf
zhangxingyue <2586429885@qq.com>
2023-01-05 00:47:34 +0800

    1

--- src/main/java/tju/ds/map/MapApplication.java
+++ src/main/java/tju/ds/map/MapApplication.java
@@ -19,4 +19,16 @@ public class MapApplication extends Application {
         primaryStage.show();
         new LoginController().show(primaryStage);
     }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        System.out.println(this + "init()...");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println(this + "stop()...");
+    }
 }
